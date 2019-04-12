34ef2c30162ba649b90ec4b8d0aace5dee4a95dd
ahchung23 <hahneol@gmail.com>
2019-04-12 03:06:04 -0400

    test env properties

--- database/index.js
+++ database/index.js
@@ -6,6 +6,8 @@ const connection = mysql.createConnection({
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   port: process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME,
+  host: process.env.RDS_HOSTNAME,
 });
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log(process.env.RDS_USERNAME, process.env.RDS_PASSWORD, process.env.RDS_PORT, process.env.RDS_DB_NAME, process.env.RDS_HOSTNAME);
