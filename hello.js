ed34f7511491a278df10b3fee7d42686ac849000
ahchung23 <hahneol@gmail.com>
2019-04-12 22:56:36 -0400

    test process.env.PORT

--- hello.js
+++ hello.js
@@ -1,26 +1,5 @@
-// console.log(process.env.RDS_USERNAME, process.env.RDS_PASSWORD, process.env.RDS_PORT, process.env.RDS_DB_NAME, process.env.RDS_HOSTNAME);
-
-// var mysql = require('mysql');
-
-// console.log('running tests \n\n\n\n\n')
-
-// var connection = mysql.createConnection({
-//   host: process.env.RDS_HOSTNAME,
-//   user: process.env.RDS_USERNAME,
-//   password: process.env.RDS_PASSWORD,
-//   port: process.env.RDS_PORT,
-//   database: process.env.RDS_DB_NAME,
-// });
 
 
+const app = require('./server/app');
 
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
-
-//   console.log('Connected to database.');
-// });
-
-// connection.end();
+console.log('=========this is port ======= ', process.env.PORT, '\n\n\n');
